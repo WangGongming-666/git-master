6f07ca85598f6637914c4e263f78235442ab95df
wgm <wgm@my.com>
2021-04-30 03:55:22 +0800

    hot3 first

--- src/a/helll.java
+++ src/a/helll.java
@@ -4,5 +4,6 @@ public class helll {
     public static void main(String[] args) {
         System.out.println("hhhhh");
         System.out.println("wwwww");
+        System.out.println("hot");
     }
 }
